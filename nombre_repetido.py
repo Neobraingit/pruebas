bbdaa4a3da39c69cf00d864168ba720a056d31cf
Neobraingit <Maervca2@icloud.com>
2023-03-01 16:53:58 +0100

    Nombre repetido

---
+++ nombre_repetido.py
@@ -0,0 +1,9 @@
+'''
+Escribir un programa que pregunte el nombre del usuario en la consola y un número entero e 
+imprima por pantalla en líneas distintas el nombre del usuario tantas veces como el número introducido.'''
+
+print ('Dime tu nombre: ')
+nombre = input()
+print ('Dime el número de veces: ')
+veces = int(input())
+print (nombre  * veces)
