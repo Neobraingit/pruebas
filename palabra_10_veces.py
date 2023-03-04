44ffe41b812d92bb962a2817eebe0098f00455b9
Neobraingit <Maervca2@icloud.com>
2023-03-04 21:08:50 +0100

    ejercicios

---
+++ palabra_10_veces.py
@@ -0,0 +1,5 @@
+# Escribir un programa que pida al usuario una palabra y la muestre por pantalla 10 veces.
+
+palabra = input('Introduce una palabra: ')
+for i in palabra in range  (1,11):
+    print (i)
