50c6d838e82b9af7d46962afdb998502d1c4253e
Neobraingit <Maervca2@icloud.com>
2023-03-05 09:09:46 +0100

    ejercicios

---
+++ edad.py
@@ -0,0 +1,4 @@
+# Escribir un programa que pregunte al usuario su edad y muestre por pantalla todos los años que ha cumplido (desde 1 hasta su edad).
+edad = int(input('Dime tu edad: '))
+for i in range(edad) :
+    print (i)
---
+++ entero_positivo.py
@@ -0,0 +1,8 @@
+'''
+Escribir un programa que pida al usuario un número entero positivo y muestre
+por pantalla todos los números impares desde 1 hasta ese número separados por comas.'''
+
+numero = int(input('Introduce un número: '))
+for i in numero % 1 in range(numero):
+    print (i)
+    
